72f663a9eaa687f823565d82f7ce427b91e3cbfa
trapcodeio <trapcode.io@gmail.com>
2020-12-27 10:18:51 +0100

    Updated Error

--- exports/config.ts
+++ exports/config.ts
@@ -2,6 +2,10 @@ import {DollarSign} from "xpresser/types";
 import {Abolish} from "abolish";
 
 export = ($: DollarSign) => ({
+    /**
+     * Abolish Instance Extender.
+     * @param Validator
+     */
     extendAbolish: (Validator: typeof Abolish): typeof Abolish => {
         // Extend Abolish here.
 
