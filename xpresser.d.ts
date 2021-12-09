d945b79c4bdb12324613efe1795a3182d9362880
trapcodeio <trapcode.io@gmail.com>
2021-12-09 09:47:36 +0100

    Use dist file

--- xpresser.d.ts
+++ xpresser.d.ts
@@ -1,5 +1,5 @@
 import "xpresser/types/http";
-import type AbolishRequestEngine from "./Extends/RequestEngine";
+import type AbolishRequestEngine from "./dist/Extends/RequestEngine";
 
 declare module "xpresser/types/http" {
     interface Http extends AbolishRequestEngine {}
