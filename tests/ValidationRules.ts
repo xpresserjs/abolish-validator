363cc7c8e1b5c8ab22df35040fce096e94669eec
trapcodeio <trapcode.io@gmail.com>
2023-08-22 20:12:37 +0100

    update types

--- tests/ValidationRules.ts
+++ tests/ValidationRules.ts
@@ -1,11 +1,14 @@
 // @ts-ignore
 import RoutesGuard from "../RoutesGuard";
+import { Abolish } from "abolish";
 
 const guard = new RoutesGuard(true);
 
-guard.post("App@login", {
+export const LoginSchema = Abolish.compileObject({
     email: "required|email",
     username: "required|alphaNumeric"
 });
+
+guard.post("App@login", LoginSchema);
 
 export default guard;
